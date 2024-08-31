0311b31122143ed45b86c2868a1d3f0d23927eb6
Saar Ben David <saarbd42@gmail.com>
2024-08-31 11:41:33 +0300

    worked a bit on the land forces screen.

--- project/game_screens/Land_forces_screen.py
+++ project/game_screens/Land_forces_screen.py
@@ -10,7 +10,9 @@ class LandForcesScreen(DecisionChoiceScreen):
         self.total_button_num = len(self.get_relevant_units())
 
     def get_button_position_list(self):
-        return [[100, 100], [300, 100], [100, 220], [300, 220], [300, 300]][:self.total_button_num]
+        units_positions = [[30, 80], [30, 180], [30, 280]][:self.total_button_num - 2]
+        attack_def_positions = [[220, 70], [220, 250]]
+        return units_positions + attack_def_positions
 
     def get_relevant_units(self):
         player_army = self.current_game_state[0]
@@ -25,5 +27,41 @@ class LandForcesScreen(DecisionChoiceScreen):
         return
 
     def draw_unit_choice_text(self, screen):
-        # Write according a specific screen
-        return
+        self.draw_the_head_line(screen)
+        # chosen_unit_list = self.which_units_did_player_choose()
+        # self.draw_button_names(screen, chosen_unit_list)
+
+    '''
+    DRAWING FUNCTIONS FOR THE TEXT IN THIS SCREEN
+    '''
+
+    def draw_the_head_line(self, screen):
+        title_text, title_rect = imf.get_text_stats("WHAT SHOULD OUR LAND FORCES DO?", 34, BLACK, [240, 30])
+        imf.draw_titles(screen, [title_text, title_rect])
+
+    def draw_button_names(self, screen, chosen_unit_list):
+        button_location_list = [[70, 175], [175, 175], [280, 175], [390, 175],
+                                [70, 300], [190, 300], [300, 300], [390, 300]]
+
+        for i in range(len(button_location_list)):
+            title_text, title_rect = self.get_button_title_details(self.all_units_names[i], button_location_list[i],
+                                                                   chosen_unit_list)
+            imf.draw_titles(screen, [title_text, title_rect])
+
+    def get_button_title_details(self, unit_name, unit_location, chosen_unit_list):
+        if unit_name in chosen_unit_list:
+            title_text, title_rect = self.get_chosen_button_title_details(unit_name, unit_location)
+        else:
+            title_text, title_rect = self.get_normal_button_title_details(unit_name, unit_location)
+        return title_text, title_rect
+
+    @staticmethod
+    def get_chosen_button_title_details(text, location):
+        title_text, title_rect = imf.get_text_stats(text, 30, LIGHT_BLUE, location)
+        return title_text, title_rect
+
+    @staticmethod
+    def get_normal_button_title_details(text, location):
+        title_text, title_rect = imf.get_text_stats(text, 30, BLACK, location)
+        return title_text, title_rect
+
--- tests/screen_tests.py
+++ tests/screen_tests.py
@@ -1,4 +1,19 @@
-from project.base_classes.Game_screen import GameScreen
+from project.army.Army import Army
+import project.game_management.enemy_units.enemy_unit_choice_functions as euc
 
-basic_screen = GameScreen()
-basic_screen.start_screen_loop()
+
+def show_action_choice_screen(armies, screen_type, needed_units):
+    screen = screen_type
+    for name in needed_units:
+        if name in armies[0].get_army_units_names():
+            return screen.start_screen_loop()
+
+
+from project.game_screens.Land_forces_screen import LandForcesScreen
+from project.config import LAND_FORCES_NAMES
+chosen_units = ["Infantry", "Tanks", "Spies", "Artillery"]
+player_army = Army(chosen_units)
+enemy_army = Army(euc.iranian_proxy())
+armies = [player_army, enemy_army]
+
+show_action_choice_screen(armies, LandForcesScreen(player_army, enemy_army), LAND_FORCES_NAMES)
